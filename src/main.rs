510aad3d3f88a78d6197d5c59f31dbd62017f075
Mitchell Valine <mitchell.valine@mirumagency.com>
2017-04-09 20:21:13 -0700

    Adds Piston dependencies.
    Adds Piston crates to Cargo.toml and starts implementing example piston
    project from [Piston README](https://github.com/PistonDevelopers/Piston-Tutorials/tree/master/getting-started) as starting point.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+pub struct App {
+    gl: GlGraphics,
+    rotation: f64,
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Main!");
 }
