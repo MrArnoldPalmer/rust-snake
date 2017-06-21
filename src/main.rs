c098b18a0b32dfce788586bf84d2cc0c261aa55c
Mitchell Valine <mitchell.valine@mirumagency.com>
2017-06-20 22:18:42 -0700

    Starting with basic window and drawing shapes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
-extern crate piston;
-extern crate graphics;
-extern crate glutin_window;
-extern crate opengl_graphics;
+extern crate piston_window;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-
-pub struct App {
-    gl: GlGraphics,
-    rotation: f64,
-}
-
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-    }
-}
+use piston_window::*;
 
 fn main() {
-    println!("Main!");
+    let mut window: PistonWindow = WindowSettings::new("Hello World", [512, 512])
+        .build()
+        .unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([0.5, 0.5, 0.5, 1.0], g);
+            rectangle([1.0, 0.0, 0.0, 1.0],
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform,
+                      g);
+        });
+    }
 }
